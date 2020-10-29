7449ac2c95ad6e4270721e594450b18e221f7a29
Diego Teixeira <diegosmteixeira@gmail.com>
2020-10-29 18:56:23 -0300

    Exception melhorada e testada

--- xadrez-console/tabuleiro/TabuleiroException.cs
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1.tabuleiro
+namespace tabuleiro
 {
-    class TabuleiroException
+    class TabuleiroException : Exception
     {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
     }
 }
